14ef6d92705b3d86b0245b5a8f182ed484cf2e85
Wolfgang Betz <wolfgang.betz@st.com>
2014-10-17 13:45:27 +0200

    Added X-NUCLEO-IKC01A1 (Battery Management Expansion Board)
    suppotring just the L6924D charger for the moment

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include "mbed.h"
+#include "x_nucleo_ikc01a1.h"
+
 #include <Ticker.h>
 
 namespace {
 	const int MS_INTERVALS = 1000;
 }
+
+static X_NUCLEO_IKC01A1 *battery_expansion_board = X_NUCLEO_IKC01A1::Instance();
 
 static Ticker timer;
 static DigitalOut led(LED1);
@@ -11,10 +15,22 @@ static DigitalIn button(USER_BUTTON);
 
 static volatile bool irq_triggered = false;
 
-static void print_char(char c)
-{
-	printf("%c", c);
-	fflush(stdout);
+/* return string describing charger state */
+static const char *getChargerCondition(void) {
+	charger_conditions_t state = 
+		battery_expansion_board->charger.GetState();
+	
+	switch (state) {
+	case CHARGER_CHARGE_IN_PROGRESS:
+		return "charging";		
+	case CHARGER_CHARGE_DONE:
+		return "done";
+	case CHARGER_IN_STAND_BY:
+		return "stand by";
+	default:
+		break;
+	}
+	return "invalid";
 }
 
 /* Called in interrupt context, therefore just set a trigger variable */
@@ -27,10 +43,14 @@ static void timer_irq(void) {
 */
 static void main_cycle(void) {
 	led = !led; // Blink
-	if(button) 
-		print_char('*');
-	else
-		print_char('-');
+	if(button == 0) { // Switch discharging state
+		printf("Switching discharging state\n");
+		battery_expansion_board->charger.discharge =
+			!battery_expansion_board->charger.discharge;
+	}
+	printf("Charge state: %s - %s\n",
+	       getChargerCondition(),
+	       battery_expansion_board->charger.discharge ? "discharging" : "!discharging");
 }
 
 /* Generic main function for enabling WFI in case of 
@@ -38,6 +58,7 @@ static void main_cycle(void) {
 */
 int main()
 {
+	printf("--- Starting new run ---\n");
 	timer.attach_us(timer_irq, MS_INTERVALS * 1000);
 
 	while (true) {
