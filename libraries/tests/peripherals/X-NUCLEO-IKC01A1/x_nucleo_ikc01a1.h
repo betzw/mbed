443b6945945bf690e0839ffe808a44126a4db211
Wolfgang Betz <wolfgang.betz@st.com>
2014-10-17 15:45:11 +0200

    Explain class X_NUCLEO_IKC01A1, its role and its nature of being a singleton

--- libraries/tests/peripherals/X-NUCLEO-IKC01A1/x_nucleo_ikc01a1.h
+++ libraries/tests/peripherals/X-NUCLEO-IKC01A1/x_nucleo_ikc01a1.h
@@ -7,6 +7,16 @@
 #include "x_nucleo_ikc01a1_charger.h"
 
 /* Classes -------------------------------------------------------------------*/
+/* Class X_NUCLEO_IKC01A1 is intended to represent the battery management 
+   expansion board with the same name, featuring basically three IPs:
+   1) a battery charger of class L6924D
+   2) a real-time clock (RTC) of class M41T62
+   3) and a gas gauge (GG) of class STC3115
+   It is intentionally implemented as a singleton because only one 
+   X_NUCLEO_IKC01A1 at a time might be deployed in a HW component stack.
+   In order to get the singleton instance you have to call class method
+   X_NUCLEO_IKC01A1::Instance().
+*/
 class X_NUCLEO_IKC01A1 {
  public:
 	static X_NUCLEO_IKC01A1* Instance();
