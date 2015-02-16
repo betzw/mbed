8006fefaa1e7a073ae480fb5c97df2d527e31ba5
rgrover1 <unknown>
2015-02-16 09:29:27 +0000

    fix license header; and delete un-necessary nrfUriBeaconConfigService.h.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-/*
- * Copyright 2014-2015 Google Inc. All rights reserved.
+/* mbed Microcontroller Library
+ * Copyright (c) 2006-2013 ARM Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
