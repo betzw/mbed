31c10331facdc811aaa46d0a909b80d2f9949fe0
Wolfgang Betz <wolfgang.betz@st.com>
2015-04-15 13:45:30 +0200

    Added component LPS25H

--- libraries/tests/peripherals/X_NUCLEO_IKS01A1/Components/lps25h/lps25h_class.h
+++ libraries/tests/peripherals/X_NUCLEO_IKS01A1/Components/lps25h/lps25h_class.h
@@ -1,10 +1,10 @@
 /**
  ******************************************************************************
- * @file    hts221_class.h
+ * @file    lps25h_class.h
  * @author  AST / EST
  * @version V0.0.1
  * @date    14-April-2015
- * @brief   Header file for component HTS221
+ * @brief   Header file for component LPS25H
  ******************************************************************************
  * @attention
  *
@@ -35,43 +35,42 @@
  ******************************************************************************
 */
 
-#ifndef __HTS221_CLASS_H
-#define __HTS221_CLASS_H
+#ifndef __LPS25H_CLASS_H
+#define __LPS25H_CLASS_H
 
 /* Includes ------------------------------------------------------------------*/
 #include "mbed.h"
 #include "DevI2C.h"
-#include "../Common/HumTemp.h"
-#include "hts221.h"
+#include "../Common/Pressure.h"
+#include "lps25h.h"
 
 /* Classes -------------------------------------------------------------------*/
-/** Class representing a HTS221 sensor component
+/** Class representing a LPS25H sensor component
  */
-class HTS221 : public HumTemp {
+class LPS25H : public Pressure {
  public:
 	/** Constructor
 	 * @param i2c device I2C to be used for communication
 	 */
-        HTS221(DevI2C &i2c) : HumTemp(), dev_i2c(i2c) {
-		T0_degC = T1_degC = H0_rh = H1_rh = 0.0;
-		T0_out = T1_out = H0_T0_out = H1_T0_out = 0;
+        LPS25H(DevI2C &i2c) : Pressure(), dev_i2c(i2c) {
+		LPS25H_SlaveAddress = LPS25H_ADDRESS_HIGH;
 	}
 	
 	/*** Interface Methods ***/
-	virtual HUM_TEMP_StatusTypeDef Init(HUM_TEMP_InitTypeDef *init_struct) {
-		return HTS221_Init(init_struct);
+	virtual PRESSURE_StatusTypeDef Init(PRESSURE_InitTypeDef *init_struct) {
+		return LPS25H_Init(init_struct);
 	}
 
-	virtual HUM_TEMP_StatusTypeDef PowerOFF(void) {
-		return HTS221_Power_OFF();
+	virtual PRESSURE_StatusTypeDef PowerOff(void) {
+		return LPS25H_PowerOff();
 	}
 
-	virtual HUM_TEMP_StatusTypeDef ReadID(uint8_t *ht_id) {
-		return HTS221_ReadID(ht_id);
+	virtual PRESSURE_StatusTypeDef ReadID(uint8_t *p_id) {
+		return LPS25H_ReadID(p_id);
 	}
 
-	virtual HUM_TEMP_StatusTypeDef Reset(void) {
-		return HTS221_RebootCmd();
+	virtual PRESSURE_StatusTypeDef Reset(void) {
+		return LPS25H_RebootCmd();
 	}
 
 	virtual void ConfigIT(uint16_t) { /* not yet implemented */ }
@@ -79,47 +78,53 @@ class HTS221 : public HumTemp {
 	virtual void DisableIT(uint8_t) { /* not yet implemented */ }
 	virtual uint8_t ITStatus(uint16_t, uint16_t) { /* not yet implemented */ return 0; }
 	virtual void ClearIT(uint16_t, uint16_t) { /* not yet implemented */ }
-
-	virtual HUM_TEMP_StatusTypeDef GetHumidity(float *pfData) {
-		return HTS221_GetHumidity(pfData);
+    
+	virtual PRESSURE_StatusTypeDef GetPressure(float *pfData) {
+		return LPS25H_GetPressure(pfData);
 	}
 
-	virtual HUM_TEMP_StatusTypeDef GetTemperature(float *pfData) {
-		return HTS221_GetTemperature(pfData);
+	virtual PRESSURE_StatusTypeDef GetTemperature(float *pfData) {
+		return LPS25H_GetTemperature(pfData);
+	}
+
+	virtual void SlaveAddrRemap(uint8_t SA0_Bit_Status) {
+		LPS25H_SlaveAddrRemap(SA0_Bit_Status);
 	}
 
 	virtual void AttachIT(void (*fptr)(void)) { /* not yet implemented */ }
 
- protected:
+protected:
 	/*** Methods ***/
-	HUM_TEMP_StatusTypeDef HTS221_Init(HUM_TEMP_InitTypeDef *HTS221_Init);
-	HUM_TEMP_StatusTypeDef HTS221_Power_OFF(void);
-	HUM_TEMP_StatusTypeDef HTS221_ReadID(uint8_t *ht_id);
-	HUM_TEMP_StatusTypeDef HTS221_RebootCmd(void);
-	HUM_TEMP_StatusTypeDef HTS221_GetHumidity(float* pfData);
-	HUM_TEMP_StatusTypeDef HTS221_GetTemperature(float* pfData);
-
-	HUM_TEMP_StatusTypeDef HTS221_Power_On(void);
-	HUM_TEMP_StatusTypeDef HTS221_Calibration(void);
+	PRESSURE_StatusTypeDef LPS25H_Init(PRESSURE_InitTypeDef *LPS25H_Init);
+	PRESSURE_StatusTypeDef LPS25H_ReadID(uint8_t *p_id);
+	PRESSURE_StatusTypeDef LPS25H_RebootCmd(void);
+	PRESSURE_StatusTypeDef LPS25H_GetPressure(float* pfData);
+	PRESSURE_StatusTypeDef LPS25H_GetTemperature(float* pfData);
+	PRESSURE_StatusTypeDef LPS25H_PowerOff(void);
+	void LPS25H_SlaveAddrRemap(uint8_t SA0_Bit_Status);
+	
+	PRESSURE_StatusTypeDef LPS25H_PowerOn(void);
+	PRESSURE_StatusTypeDef LPS25H_I2C_ReadRawPressure(uint32_t *raw_press);
+	PRESSURE_StatusTypeDef LPS25H_I2C_ReadRawTemperature(int16_t *raw_data);
 
 	/**
-	 * @brief  Configures HTS221 interrupt lines for NUCLEO boards
+	 * @brief  Configures LPS25H interrupt lines for NUCLEO boards
 	 * @param  None
 	 * @retval None
 	 */
-	void HTS221_IO_ITConfig(void)
+	void LPS25H_IO_ITConfig(void)
 	{
 		/* To be implemented */
 	}
 
 	/**
-	 * @brief  Configures HTS221 I2C interface
+	 * @brief  Configures LPS25H I2C interface
 	 * @param  None
-	 * @retval HUM_TEMP_OK in case of success, an error code otherwise
+	 * @retval PRESSURE_OK in case of success, an error code otherwise
 	 */
-	HUM_TEMP_StatusTypeDef HTS221_IO_Init(void)
+	PRESSURE_StatusTypeDef LPS25H_IO_Init(void)
 	{
-		return HUM_TEMP_OK; /* done in constructor */
+		return PRESSURE_OK; /* done in constructor */
 	}
 
 	/**
@@ -127,19 +132,19 @@ class HTS221 : public HumTemp {
 	 * @param  pBuffer: pointer to data to be read.
 	 * @param  RegisterAddr: specifies internal address register to read from.
 	 * @param  NumByteToRead: number of bytes to be read.
-	 * @retval HUM_TEMP_OK if ok, HUM_TEMP_ERROR if an I2C error has occured
+	 * @retval PRESSURE_OK if ok, PRESSURE_ERROR if an I2C error has occured
 	 */
-	HUM_TEMP_StatusTypeDef HTS221_IO_Read(uint8_t* pBuffer, 
+	PRESSURE_StatusTypeDef LPS25H_IO_Read(uint8_t* pBuffer, 
 					      uint8_t RegisterAddr, uint16_t NumByteToRead)
 	{
 		int ret = dev_i2c.i2c_read(pBuffer,
-					   HTS221_ADDRESS,
+					   LPS25H_SlaveAddress,
 					   RegisterAddr,
 					   NumByteToRead);
 		if(ret != 0) {
-			return HUM_TEMP_ERROR;
+			return PRESSURE_ERROR;
 		}
-		return HUM_TEMP_OK;
+		return PRESSURE_OK;
 	}
 	
 	/**
@@ -149,34 +154,24 @@ class HTS221 : public HumTemp {
 	 * @param  NumByteToWrite: number of bytes to write.
 	 * @retval 0 if ok, -1 if an I2C error has occured
 	 */
-	HUM_TEMP_StatusTypeDef HTS221_IO_Write(uint8_t* pBuffer, 
+	PRESSURE_StatusTypeDef LPS25H_IO_Write(uint8_t* pBuffer, 
 					       uint8_t RegisterAddr, uint16_t NumByteToWrite)
 	{
 		int ret = dev_i2c.i2c_write(pBuffer,
-					    HTS221_ADDRESS,
+					    LPS25H_SlaveAddress,
 					    RegisterAddr,
 					    NumByteToWrite);
 		if(ret != 0) {
-			return HUM_TEMP_ERROR;
+			return PRESSURE_ERROR;
 		}
-		return HUM_TEMP_OK;
+		return PRESSURE_OK;
 	}
 	
 	/*** Instance Variables ***/
 	/* IO Device */
 	DevI2C &dev_i2c;
 
-	/* Temperature in degree for calibration  */
-	float T0_degC, T1_degC;
-	
-	/* Output temperature value for calibration */
-	int16_t T0_out, T1_out;
-	
-	/* Humidity for calibration  */
-	float H0_rh, H1_rh;
-	
-	/* Output Humidity value for calibration */
-	int16_t H0_T0_out, H1_T0_out;
+	uint8_t LPS25H_SlaveAddress;
 };
 
-#endif // __HTS221_CLASS_H
+#endif // __LPS25H_CLASS_H
