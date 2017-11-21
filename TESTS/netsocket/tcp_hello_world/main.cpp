96c569ec6ce08bba1b78996d7c615ca7f943c211
Wolfgang Betz <wolfgang.betz@st.com>
2017-11-21 08:46:24 +0100

    Correct tests `tcp_hello_world` & `socket_sigio`
    A call to
    
        `TCPSocket::recv(void *data, nsapi_size_t size)`
    
    returns, following the mbed documentation, the number of received bytes on
    success, and a negative error code on failure.
    
    So in case of success, the return value depends on both the value of parameter
    `size` but also on the amount of data already available. This means, that the
    value returned can be lower than or equal to the `size` of the `data` buffer
    passed as argument to the call.
    
    Therefore, in the cases of `test_tcp_hello_world()` & `find_substring()`
    (i.e. test `socket_sigio`), the calls to `TCPSocket::recv()` might return from
    one byte up to `sizeof(buffer) - 1` (i.e. 511) bytes for each single call,
    while the tests expect to receive the whole response string with a single call.
    
    This commit applies a fix to this situation by implementing a receive loop
    which exits once there is no data anymore available to be read from the socket.

--- TESTS/netsocket/tcp_hello_world/main.cpp
+++ TESTS/netsocket/tcp_hello_world/main.cpp
@@ -74,20 +74,28 @@ void test_tcp_hello_world() {
         sock.send(buffer, strlen(buffer));
 
         // Server will respond with HTTP GET's success code
-        const int ret = sock.recv(buffer, sizeof(buffer) - 1);
-        buffer[ret] = '\0';
+        int len = 0;
+        int ret;
+        while((ret = sock.recv(buffer+len, sizeof(buffer) - 1 - len)) > 0) {
+            len += ret;
+            sock.set_timeout(0);
+        }
+        if(len >= 0)
+            buffer[len] = '\0';
+        else
+            buffer[0] = '\0';
 
         // Find 200 OK HTTP status in reply
-        bool found_200_ok = find_substring(buffer, buffer + ret, HTTP_OK_STR, HTTP_OK_STR + strlen(HTTP_OK_STR));
+        bool found_200_ok = find_substring(buffer, buffer + len, HTTP_OK_STR, HTTP_OK_STR + strlen(HTTP_OK_STR));
         // Find "Hello World!" string in reply
-        bool found_hello = find_substring(buffer, buffer + ret, HTTP_HELLO_STR, HTTP_HELLO_STR + strlen(HTTP_HELLO_STR));
+        bool found_hello = find_substring(buffer, buffer + len, HTTP_HELLO_STR, HTTP_HELLO_STR + strlen(HTTP_HELLO_STR));
 
         TEST_ASSERT_TRUE(found_200_ok);
         TEST_ASSERT_TRUE(found_hello);
 
         if (found_200_ok && found_hello) result = true;
 
-        printf("HTTP: Received %d chars from server\r\n", ret);
+        printf("HTTP: Received %d chars from server\r\n", len);
         printf("HTTP: Received 200 OK status ... %s\r\n", found_200_ok ? "[OK]" : "[FAIL]");
         printf("HTTP: Received '%s' status ... %s\r\n", HTTP_HELLO_STR, found_hello ? "[OK]" : "[FAIL]");
         printf("HTTP: Received message:\r\n");
